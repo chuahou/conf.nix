abc102f111202d836da01718d93eef0ebefb40a9
Chua Hou <human+github@chuahou.dev>
2020-12-29 19:40:52 +0800

    rcfiles: remove
    - We will use home-manager and reconstruct our setup

--- rcfiles/tag-i3/config/i3/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2020 Chua Hou
-#
-# Starts everything in startup.d.
-
-for script in $(dirname $0)/startup.d/*.sh; do
-	$script &
-done
