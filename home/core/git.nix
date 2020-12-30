fb72a38257cd9cbf2eb1737ab4aa4778f8e95aa9
Chua Hou <human+github@chuahou.dev>
2020-12-30 14:33:14 +0800

    home/me: reorganize information

--- home/core/git.nix
+++ home/core/git.nix
@@ -7,9 +7,9 @@ in
     programs.git = {
       enable = true;
       userName = me.name;
-      userEmail = me.email;
+      userEmail = me.github.email;
       signing = {
-        key = "314311D4CE92CC07DD5BDDC8A7F9181F143648FD";
+        key = me.github.gpgKey;
         signByDefault = true;
       };
       extraConfig = {
--- home/lib/me.nix
+++ home/lib/me.nix
@@ -1,4 +1,9 @@
 {
   name = "Chua Hou";
-  email = "human+github@chuahou.dev";
+
+  github = {
+    username = "chuahou";
+    email    = "human+github@chuahou.dev";
+    gpgKey   = "314311D4CE92CC07DD5BDDC8A7F9181F143648FD";
+  };
 }
