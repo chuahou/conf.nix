29eeee3d06bc8f09657bfef57723c524bd00d7a1
Chua Hou <human+github@chuahou.dev>
2021-01-01 18:48:03 +0800

    home/vim: add all plugins :)

--- home/core/vim.nix
+++ home/core/vim.nix
@@ -15,15 +15,81 @@ in
           " we use the full configuration copied
           set runtimepath^=${configDir} runtimepath+=${configDir}/after
           source ${configDir}/init.vim
+
+          " additional plugin customization
+
+          " vim-airline
+          let g:airline_theme = 'angr'
+          let g:airline#extensions#whitespace#mixed_indent_algo = 1
+          let g:airline#extensions#checks = [
+            \ 'indent', 'long', 'trailing', 'mixed-indent-file', 'conflicts' ]
+          let g:airline#extensions#whitespace#skip_indent_check_ft = {
+            \ 'vim': ['trailing'],
+            \ }
+
+          " faster updates for coc/vim-gitgutter etc
+          set updatetime=100
+
+          " coc configuration
+          " original example at
+          " https://github.com/neoclide/coc.nvim#example-vim-configuration
+
+          " more space to display
+          set cmdheight=2
+
+          " trigger completion with <C-space>
+          inoremap <silent><expr> <C-space> coc#refresh()
+
+          " activate actions with <C-space>
+          nmap <C-space> :CocAction<CR>
+
+          " diagnostics navigation
+          nmap <silent> [g <Plug>(coc-diagnostic-prev)
+          nmap <silent> ]g <Plug>(coc-diagnostic-next)
+
+          " code navigation
+          nmap <silent> gd <Plug>(coc-definition)
+          nmap <silent> gy <Plug>(coc-type-definition)
+          nmap <silent> gi <Plug>(coc-implementation)
+          nmap <silent> gr <Plug>(coc-references)
+
+          " use K to show documentation in preview window
+          nnoremap <silent> K :call <SID>show_documentation()<CR>
+          function! s:show_documentation()
+              if (index(['vim','help'], &filetype) >= 0)
+                  execute 'h '.expand('<cword>')
+              elseif (coc#rpc#ready())
+                  call CocActionAsync('doHover')
+              else
+                  execute '!' . &keywordprg . " " . expand('<cword>')
+              endif
+          endfunction
         '';
         packages.myVimPackage = with pkgs.vimPlugins; {
-          start = [];
-          opt = [];
+          start = [
+            # language plugins
+            coc-nvim
+            haskell-vim
+            vim-nix
+
+            # alignment
+            tabular
+
+            # vim-airline
+            vim-airline
+            vim-airline-themes
+
+            # git sign column
+            vim-gitgutter
+          ];
         };
       };
 
       viAlias      = true;
       vimAlias     = true;
       vimdiffAlias = true;
+
+      # for coc.nvim
+      withNodeJs = true;
     };
   }
