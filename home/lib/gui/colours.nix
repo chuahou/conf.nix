7b6021c76f02b04f20f47e9148879972397b087c
Chua Hou <human+github@chuahou.dev>
2021-01-01 13:10:28 +0800

    home/colours: add common colours

---
+++ home/lib/gui/colours.nix
@@ -0,0 +1,53 @@
+# common colours
+
+{
+  black   = "#222222";
+  red     = "#D14949";
+  green   = "#48874F";
+  yellow  = "#AFA75A";
+  blue    = "#599797";
+  magenta = "#8F6089";
+  cyan    = "#5C9FA8";
+  white   = "#8C8C8C";
+
+  # bright variant
+  bright = {
+    black   = "#444444";
+    red     = "#FF6D6D";
+    green   = "#89FF95";
+    yellow  = "#FFF484";
+    blue    = "#97DDFF";
+    magenta = "#FDAAF2";
+    cyan    = "#85F5DA";
+    white   = "#E9E9E9";
+  };
+
+  # core colours translucently laid over gray.1
+  gray = {
+    red     = "#5F4351";
+    green   = "#3E5651";
+    yellow  = "#686754";
+    blue    = "#3F525D";
+    magenta = "#4B465A";
+    cyan    = "#405461";
+    white   = "#585B64";
+  };
+
+  # terminal colours
+  term = {
+    bg        = "#1B1B1B";
+    fg        = "#BEAF8A";
+    fg-bright = "#CFBF9B";
+  };
+
+  gray = {
+    "0" = "#2E3440";
+    "1" = "#3B4252";
+    "2" = "#434C5E";
+    "3" = "#7B8394";
+    "4" = "#D8DEE9";
+    "5" = "#E5E9F0";
+    "6" = "#ECEFF4";
+    "7" = "#ECEFF4";
+  };
+}
