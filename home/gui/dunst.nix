6ee8bc129a9430bbd448e5208855d1def68aad1c
Chua Hou <human+github@chuahou.dev>
2021-01-10 22:25:43 +0800

    dunst: add configuration

---
+++ home/gui/dunst.nix
@@ -0,0 +1,55 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2021 Chua Hou
+
+{ config, lib, ... }:
+
+{
+  services.dunst = {
+    enable = true;
+
+    settings =
+      let
+        colours = import ../lib/gui/colours.nix;
+      in rec {
+        global = {
+          follow             = "keyboard";
+          geometry           = "0x5-18+45";
+          transparency       = 15;
+          separator_height   = 2;
+          padding            = 8;
+          horizontal_padding = 8;
+          frame_width        = 2;
+          frame_color        = colours.gray."7";
+          separator_color    = "frame";
+          sort               = "yes";
+          idle_threshold     = 300;
+          font               = "Iosevka Regular 12";
+          markup             = "full";
+          format             = "<u><i>%a</i></u>\\n<b>%s</b>\\n%b";
+          show_age_threshold = 60;
+          word_wrap          = true;
+        };
+        urgency_low = {
+          background = colours.gray."1";
+          foreground = colours.gray."7";
+          timeout    = 10;
+        };
+        urgency_normal = {
+          inherit (urgency_low) background foreground timeout;
+        };
+        urgency_critical = {
+          inherit (urgency_low) background foreground;
+          frame_color = "#FF0000";
+          timeout     = 0;
+        };
+        shortcuts =
+          let
+            mod = lib.toLower config.xsession.windowManager.i3.config.modifier;
+          in {
+            close     = "${mod}+bracketright";
+            history   = "${mod}+bracketleft";
+            close_all = "${mod}+backslash";
+          };
+      };
+  };
+}
