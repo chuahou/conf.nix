da9978d7bae9ebad01fd03df11a02e9a96773fd7
Chua Hou <human+github@chuahou.dev>
2021-01-10 18:21:03 +0800

    home/xsession: add xconfig.sh

--- home/gui/xsession.nix
+++ home/gui/xsession.nix
@@ -1,11 +1,57 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2021 Chua Hou
 
-{ config, ... }:
+{ config, pkgs, ... }:
 
+let
+  xconfigScript =
+    let
+      xrandr = "${pkgs.xorg.xrandr}/bin/xrandr";
+      xset   = "${pkgs.xorg.xset}/bin/xset";
+      xinput = "${pkgs.xorg.xinput}/bin/xinput";
+    in
+      pkgs.writeShellScriptBin "xconfig.sh" ''
+        # force full composition pipeline for nvidia to prevent tearing
+        nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
+
+        # check if external monitor connected to HDMI-0
+        if [ $(${xrandr} -q | grep -c "HDMI-0 connected") -gt 0 ]; then
+          # turn off eDP-1-1 and connect only to HDMI-0
+          ${xrandr} --output HDMI-0 --auto --primary --output eDP-1-1 --off
+        else
+          ${xrandr} --output eDP-1-1 --auto --primary --output HDMI-0 --off
+        fi
+
+        # disable mouse acceleration
+        ${xset} mouse 0 0
+        for mouse in $(${xinput} --list | sed -n 's/^.*[Mm]ouse.*id=\([0-9]\+\).*$/\1/p')
+        do
+          if [ $(${xinput} list-props $mouse | \
+            grep -c "libinput Accel Profile Enabled") -ge 1 ]; then
+            ${xinput} set-prop $mouse "libinput Accel Profile Enabled" 0 1
+          fi
+        done
+
+        # disable X power saving
+        ${xset} s off -dpms
+
+        # disable stream restore module if loaded
+        [ $(pactl list short modules | grep module-stream-restore -c || true) -gt 0 ] &&
+          pactl unload-module module-stream-restore
+      '';
+in
 {
+  home.packages = [
+    # shell script that does all X-related configuration
+    xconfigScript
+  ];
+
   xsession = {
     enable = false;
+
+    initExtra = ''
+      ${xconfigScript}/bin/xconfig.sh
+    '';
 
     windowManager.i3 =
       let
@@ -132,6 +178,9 @@
               "${mod}+space"       = "exec ${rofi} -show drun";
               "${mod}+Shift+space" = "exec ${rofi} -show run";
               "${mod}+Ctrl+space"  = "exec ${rofi} -show window";
+
+              # redo xconfig
+              "${mod}+Shift+Ctrl+z" = "exec ${xconfigScript}/bin/xconfig.sh";
             } //
 
             # workspace navigation
