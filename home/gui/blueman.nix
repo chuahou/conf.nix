25c776850d9dd073e85b718b78c75ff4e2d299c8
Chua Hou <human+github@chuahou.dev>
2021-01-11 21:36:56 +0800

    home/blueman: add dconf as dependency
    - Set dconf option so blueman does not ask every time

--- home/gui/blueman.nix
+++ home/gui/blueman.nix
@@ -1,8 +1,13 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2021 Chua Hou
 
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.blueman-applet.enable = true;
+
+  home.packages = [ pkgs.dconf ];
+
+  # otherwise blueman-manager will keep prompting for this
+  dconf.settings."org/blueman/plugins/powermanager".auto-power-on = false;
 }
