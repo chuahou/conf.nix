c2a20b0201741e9753408c39cd53ca0a3019008d
Chua Hou <human+github@chuahou.dev>
2021-01-14 21:03:37 +0800

    home/emacs: add systemd unit/timer for org-gcal

--- home/core/emacs.nix
+++ home/core/emacs.nix
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2021 Chua Hou
 
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   programs.emacs = {
@@ -11,4 +11,29 @@
   };
 
   home.file.".emacs".source = ../res/emacs.el;
+
+  # systemd timer unit to run gcal script every 5 minutes
+  systemd.user =
+    let
+      name = "org-gcal";
+    in {
+      services.${name} = {
+        Service = {
+          Type      = "oneshot";
+          ExecStart = "${pkgs.writeShellScript "org-gcal-systemd" ''
+            ${(import ../../lib { inherit lib; }).mkPath (with pkgs; [
+              bash curl gawk
+            ])}
+            ${config.home.homeDirectory}/org/gcal/gcal.sh
+          ''}";
+        };
+      };
+      timers.${name} = {
+        Timer = {
+          OnUnitActiveSec = 5 * 60;
+          OnBootSec       = 0;
+        };
+        Install.WantedBy = [ "timers.target" ];
+      };
+    };
 }
