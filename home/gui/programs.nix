2d6637b9ccd1e220d574e6c37f2618a031560bf3
Chua Hou <human+github@chuahou.dev>
2021-01-14 21:47:55 +0800

    home/mimeapps: move to programs.nix, add zathura

--- home/gui/programs.nix
+++ home/gui/programs.nix
@@ -35,4 +35,14 @@
 
   services.dropbox.enable   = true;
   services.syncthing.enable = true;
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "x-scheme-handler/http"  = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "text/html"              = "firefox.desktop";
+      "application/pdf"        = "org.pwmt.zathura.desktop";
+    };
+  };
 }
