eb0228cfa5550aebe792e33b8cb2bb759de855bf
Chua Hou <human+github@chuahou.dev>
2021-01-22 20:58:09 +0800

    scripts/update.sh: write script that updates all relevant things

---
+++ scripts/update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2021 Chua Hou
+#
+# Updates everything.
+
+# update commits
+nix flake update --recreate-lock-file
+sudo nix-channel --update
+
+# upgrade NixOS, home and nix-env packages
+sudo nixos-rebuild switch --flake $(dirname $0)/..#
+$(dirname $0)/home-switch.sh
+$(dirname $0)/nix-env.sh -u
