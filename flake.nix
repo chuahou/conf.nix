9354cdc643576e327761677773f0034a77511db3
Chua Hou <human+github@chuahou.dev>
2021-06-07 01:44:04 +0800

    flake: update 20.09 -> 21.05
    - Compatibility fix commits to follow.

--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
 
 {
   inputs = {
-    nixpkgs        = { url = "nixpkgs/nixos-20.09"; };
+    nixpkgs        = { url = "nixpkgs/nixos-21.05"; };
     unstable       = { url = "nixpkgs/nixpkgs-unstable"; };
     nixos-hardware = { url = "github:NixOS/nixos-hardware"; };
     home-manager = {
-      url = "github:nix-community/home-manager/release-20.09";
+      url = "github:nix-community/home-manager/release-21.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
