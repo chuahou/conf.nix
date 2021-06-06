62a5c40fbd737dd93ad7dcb0ad61b80039e435a8
Chua Hou <human+github@chuahou.dev>
2021-06-07 01:45:08 +0800

    home/vim: stop using deprecated 'configure', move config directory
    - The old config directory conflicts with new home-manager
      configuration's module which also wants to write to the same location.

--- home/core/vim.nix
+++ home/core/vim.nix
@@ -3,10 +3,12 @@
 
 { config, pkgs, ... }:
 
-let configDir = "${config.xdg.configHome}/nvim";
+let
+  configLocation = "nvim/copied";
+  configDir = "${config.xdg.configHome}/${configLocation}";
 in {
   # copy entire nvim configuration directory
-  xdg.configFile.nvim.source = ../res/nvim;
+  xdg.configFile.${configLocation}.source = ../res/nvim;
 
   # copy editorconfig
   home.file.".editorconfig".source = ../res/editorconfig;
@@ -14,139 +16,135 @@ in {
   programs.neovim = {
     enable = true;
 
-    configure = {
-      customRC = ''
-        " faster updates for coc/vim-gitgutter etc
-        set updatetime=100
+    extraConfig = ''
+      " faster updates for coc/vim-gitgutter etc
+      set updatetime=100
 
-        " we use the full configuration copied
-        set runtimepath^=${configDir} runtimepath+=${configDir}/after
-        source ${configDir}/init.vim
+      " we use the full configuration copied
+      set runtimepath^=${configDir} runtimepath+=${configDir}/after
+      source ${configDir}/init.vim
 
-        "-------------"
-        " vim-airline "
-        "-------------"
-        let g:airline_theme = 'angr'
-        let g:airline#extensions#whitespace#mixed_indent_algo = 1
-        let g:airline#extensions#checks = [
-          \ 'indent', 'long', 'trailing', 'mixed-indent-file', 'conflicts' ]
-        let g:airline#extensions#whitespace#skip_indent_check_ft = {
-          \ 'vim': ['trailing'],
-          \ }
+      "-------------"
+      " vim-airline "
+      "-------------"
+      let g:airline_theme = 'angr'
+      let g:airline#extensions#whitespace#mixed_indent_algo = 1
+      let g:airline#extensions#checks = [
+        \ 'indent', 'long', 'trailing', 'mixed-indent-file', 'conflicts' ]
+      let g:airline#extensions#whitespace#skip_indent_check_ft = {
+        \ 'vim': ['trailing'],
+        \ }
 
-        " custom symbols for vim-airline
-        if !exists('g:airline_symbols')
-          let g:airline_symbols = {}
+      " custom symbols for vim-airline
+      if !exists('g:airline_symbols')
+        let g:airline_symbols = {}
+      endif
+      let g:airline_symbols.maxlinenr = ' ln'
+
+      "----------"
+      " coc.nvim "
+      "----------"
+
+      " original example at
+      " https://github.com/neoclide/coc.nvim#example-vim-configuration
+
+      " more space to display
+      set cmdheight=2
+
+      " trigger completion with <C-space>
+      inoremap <silent><expr> <C-space> coc#refresh()
+
+      " activate actions with <C-space>
+      nmap <C-space> :CocAction<CR>
+
+      " diagnostics navigation
+      nmap <silent> [g <Plug>(coc-diagnostic-prev)
+      nmap <silent> ]g <Plug>(coc-diagnostic-next)
+
+      " code navigation
+      nmap <silent> gd <Plug>(coc-definition)
+      nmap <silent> gy <Plug>(coc-type-definition)
+      nmap <silent> gi <Plug>(coc-implementation)
+      nmap <silent> gr <Plug>(coc-references)
+
+      " use K to show documentation in preview window
+      nnoremap <silent> K :call <SID>show_documentation()<CR>
+      function! s:show_documentation()
+        if (index(['vim','help'], &filetype) >= 0)
+          execute 'h '.expand('<cword>')
+        elseif (coc#rpc#ready())
+          call CocActionAsync('doHover')
+        else
+          execute '!' . &keywordprg . " " . expand('<cword>')
         endif
-        let g:airline_symbols.maxlinenr = ' ln'
+      endfunction
 
-        "----------"
-        " coc.nvim "
-        "----------"
+      " add instantRst to path for InstantRst plugin
+      let $PATH .= ':' . '${pkgs.instantRstPy}/bin'
 
-        " original example at
-        " https://github.com/neoclide/coc.nvim#example-vim-configuration
+      " use smdv in vim-instant-markdown
+      let $PATH .= ':' . '${pkgs.smdv}/bin'
+      let g:instant_markdown_python = 1
+      let g:instant_markdown_autostart = 0 " manual startup
 
-        " more space to display
-        set cmdheight=2
+      "--------"
+      " vimtex "
+      "--------"
 
-        " trigger completion with <C-space>
-        inoremap <silent><expr> <C-space> coc#refresh()
+      " use LaTeX flavour by default
+      let g:tex_flavor = 'latex'
 
-        " activate actions with <C-space>
-        nmap <C-space> :CocAction<CR>
+      " disable alignment of ampersands
+      let g:vimtex_indent_on_ampersands = 0
 
-        " diagnostics navigation
-        nmap <silent> [g <Plug>(coc-diagnostic-prev)
-        nmap <silent> ]g <Plug>(coc-diagnostic-next)
+      " disable imaps mappings since ` is used often
+      let g:vimtex_imaps_enabled = 0
 
-        " code navigation
-        nmap <silent> gd <Plug>(coc-definition)
-        nmap <silent> gy <Plug>(coc-type-definition)
-        nmap <silent> gi <Plug>(coc-implementation)
-        nmap <silent> gr <Plug>(coc-references)
+      "-------------------"
+      " vim-pandoc-syntax "
+      "-------------------"
 
-        " use K to show documentation in preview window
-        nnoremap <silent> K :call <SID>show_documentation()<CR>
-        function! s:show_documentation()
-          if (index(['vim','help'], &filetype) >= 0)
-            execute 'h '.expand('<cword>')
-          elseif (coc#rpc#ready())
-            call CocActionAsync('doHover')
-          else
-            execute '!' . &keywordprg . " " . expand('<cword>')
-          endif
-        endfunction
+      " load vim-pandoc-syntax
+      augroup pandoc_syntax
+        au! BufNewFile,BufFilePre,BufRead *.md set ft=markdown.pandoc
+      augroup END
 
-        " add instantRst to path for InstantRst plugin
-        let $PATH .= ':' . '${pkgs.instantRstPy}/bin'
+      " don't conceal
+      let g:pandoc#syntax#conceal#use = 0
+    '';
 
-        " use smdv in vim-instant-markdown
-        let $PATH .= ':' . '${pkgs.smdv}/bin'
-        let g:instant_markdown_python = 1
-        let g:instant_markdown_autostart = 0 " manual startup
+    plugins = with pkgs.vimPlugins; [
+      # language plugins
+      pkgs.coc-nvim
+      pkgs.coc-clangd
+      haskell-vim
+      vim-nix
+      vimtex
+      vim-pandoc-syntax
+      purescript-vim
 
-        "--------"
-        " vimtex "
-        "--------"
+      # alignment
+      tabular
 
-        " use LaTeX flavour by default
-        let g:tex_flavor = 'latex'
+      # vim-airline
+      vim-airline
+      vim-airline-themes
 
-        " disable alignment of ampersands
-        let g:vimtex_indent_on_ampersands = 0
+      # git sign column
+      vim-gitgutter
 
-        " disable imaps mappings since ` is used often
-        let g:vimtex_imaps_enabled = 0
+      # InstantRst
+      pkgs.instantRstVim
 
-        "-------------------"
-        " vim-pandoc-syntax "
-        "-------------------"
+      # vim-instant-markdown
+      pkgs.vim-instant-markdown
 
-        " load vim-pandoc-syntax
-        augroup pandoc_syntax
-          au! BufNewFile,BufFilePre,BufRead *.md set ft=markdown.pandoc
-        augroup END
+      # NERDtree
+      nerdtree
 
-        " don't conceal
-        let g:pandoc#syntax#conceal#use = 0
-      '';
-
-      packages.myVimPackage = with pkgs.vimPlugins; {
-        start = [
-          # language plugins
-          pkgs.coc-nvim
-          pkgs.coc-clangd
-          haskell-vim
-          vim-nix
-          vimtex
-          vim-pandoc-syntax
-          purescript-vim
-
-          # alignment
-          tabular
-
-          # vim-airline
-          vim-airline
-          vim-airline-themes
-
-          # git sign column
-          vim-gitgutter
-
-          # InstantRst
-          pkgs.instantRstVim
-
-          # vim-instant-markdown
-          pkgs.vim-instant-markdown
-
-          # NERDtree
-          nerdtree
-
-          # editorconfig
-          editorconfig-vim
-        ];
-      };
-    };
+      # editorconfig
+      editorconfig-vim
+    ];
 
     viAlias      = true;
     vimAlias     = true;
