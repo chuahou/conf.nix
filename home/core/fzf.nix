5cd606b29f759138a066fde018b256b37d9353ad
Chua Hou <human+github@chuahou.dev>
2021-11-08 18:55:36 +0000

    home/fzf: use fd, don't show hidden files

--- home/core/fzf.nix
+++ home/core/fzf.nix
@@ -4,10 +4,20 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ fd fzf ];
+  home.packages = with pkgs; [ fzf ];
 
   programs.zsh.initExtra = ''
     source $(fzf-share)/completion.zsh
     source $(fzf-share)/key-bindings.zsh
+
+    # Use fd instead of find, don't show hidden files.
+    _fzf_compgen_path() {
+      echo "$1"
+      ${pkgs.fd}/bin/fd --follow . "$1" 2> /dev/null
+    }
+    _fzf_compgen_dir() {
+      echo "$1"
+      ${pkgs.fd}/bin/fd --type d --follow . "$1" 2> /dev/null
+    }
   '';
 }
