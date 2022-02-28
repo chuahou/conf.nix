01f1c5766eb02a5edeb9e9e89a097b74f6b84de2
Chua Hou <human+github@chuahou.dev>
2022-02-28 10:12:59 +0000

    nixos/CH-22T: use nixos options to configure thinkfan
    - The out-of-order YAML wasn't actually a problem, it was the bias settings.

--- nixos/CH-22T/hardware.nix
+++ nixos/CH-22T/hardware.nix
@@ -40,41 +40,47 @@
   services.tlp.enable = true;
 
   # Thinkfan config.
-  # We manually write YAML file since the generator generates things out of
-  # order which seems to create some issues...
   boot.extraModprobeConfig = "options thinkpad_acpi fan_control=1";
-  services.thinkfan.enable = true;
-  systemd.services.thinkfan.environment.THINKFAN_ARGS = lib.mkForce
-    "-b-10 -c ${pkgs.writeTextFile {
-      name = "thinkfan.yaml";
-      text = ''
-        sensors:
-          - hwmon: /sys/class/hwmon
-            name: coretemp
-            indices: [ 1, 2, 3 ]
-          - hwmon: /sys/devices/platform/thinkpad_hwmon/hwmon
-            indices: [ 1 ]
-          - tpacpi: /proc/acpi/ibm/thermal
-            indices: [ 0 ]
-          - hwmon: /sys/devices/pci0000:00/0000:00:1d.2/0000:3e:00.0/hwmon
-            indices: [ 1 ]
-        fans:
-          - tpacpi: /proc/acpi/ibm/fan
-        levels:
-          - speed: 0
-            upper_limit: [ 65, 65, 65, 65, 65, 30 ]
-          - speed: 1
-            lower_limit: [ 57, 57, 57, 57, 57, 28 ]
-            upper_limit: [ 70, 70, 70, 70, 70, 30 ]
-          - speed: 2
-            lower_limit: [ 50, 50, 50, 50, 50, 28 ]
-            upper_limit: [ 75, 75, 75, 75, 75, 30 ]
-          - speed: level auto
-            lower_limit: [ 55, 55, 55, 55, 55, 28 ]
-            upper_limit: [ 80, 80, 80, 80, 80, 30 ]
-          - speed: 7
-            lower_limit: [ 75, 75, 75, 75, 75, 28 ]
-            upper_limit: [ 255, 255, 255, 255, 255, 255 ]
-      '';
-    }}";
+  services.thinkfan = {
+    enable = true;
+    extraArgs = [ "-b-10" ];
+    sensors = [
+      {
+        type = "hwmon";
+        query = "/sys/class/hwmon";
+        name = "coretemp";
+        indices = [ 1 2 3 ];
+      }
+      {
+        type = "hwmon";
+        query = "/sys/devices/platform/thinkpad_hwmon/hwmon";
+        indices = [ 1 ];
+      }
+      {
+        type = "tpacpi";
+        query = "/proc/acpi/ibm/thermal";
+        indices = [ 0 ];
+      }
+      { # NVME drive.
+        type = "hwmon";
+        query = "/sys/devices/pci0000:00/0000:00:1d.2/0000:3e:00.0/hwmon";
+        indices = [ 1 ];
+        correction = [ 25 ]; # Generous correction since it does not heat up
+                             # much typically.
+      }
+    ];
+    fans = [
+      {
+        type = "tpacpi";
+        query = "/proc/acpi/ibm/fan";
+      }
+    ];
+    levels = let a = "level auto"; in [
+      [ 0   0  65 ]
+      [ 1  57  70 ]
+      [ 2  50  75 ]
+      [ a  55  80 ]
+      [ 7  75 255 ]
+    ];
+  };
 }
