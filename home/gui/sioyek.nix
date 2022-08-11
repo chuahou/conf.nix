1741617eff80b77f1700998290caeee69d4e46a7
Chua Hou <human+github@chuahou.dev>
2022-08-11 17:59:03 +0800

    home/sioyek: migrate to home-manager module
    - Progresses sioyek on #9.

--- home/gui/sioyek.nix
+++ home/gui/sioyek.nix
@@ -4,32 +4,29 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ sioyek ];
+  programs.sioyek = {
+    enable = true;
+    config = {
+      background_color = "0.1 0.1 0.1"; # Black background.
+      link_highlight_color = "0.8 0.0 0.0"; # Red highlight for links.
+    };
+    bindings = {
+      # Vim-style movement.
+      move_right = "h";
+      move_down = "j";
+      move_up = "k";
+      move_left = "l";
+      screen_down = "<C-d>";
+      screen_up = "<C-u>";
+      next_page = "<S-j>";
+      previous_page = "<S-k>";
 
-  xdg.configFile."sioyek/prefs_user.config".text = ''
-    # Black background.
-    background_color 0.1 0.1 0.1
+      # Use = to reset zoom.
+      fit_to_page_width = "=";
 
-    # Highlink links with red.
-    link_highlight_color 0.8 0.0 0.0
-  '';
-
-  xdg.configFile."sioyek/keys_user.config".text = ''
-    # Vim-style movement.
-    move_right    h
-    move_down     j
-    move_up       k
-    move_left     l
-    screen_down   <C-d>
-    screen_up     <C-u>
-    next_page     <S-j>
-    previous_page <S-k>
-
-    # Use = to reset zoom.
-    fit_to_page_width =
-
-    # Jump through states.
-    next_state <C-i>
-    prev_state <C-o>
-  '';
+      # Jump through states.
+      next_state = "<C-i>";
+      prev_state = "<C-o>";
+    };
+  };
 }
