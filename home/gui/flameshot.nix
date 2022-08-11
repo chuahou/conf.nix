b1f4639728840cb3502903e37ecb1d5e49f6a98c
Chua Hou <human+github@chuahou.dev>
2022-08-11 18:03:52 +0800

    home/flameshot: migrate to home-manager module
    - Progresses flameshot on #9.

--- home/gui/flameshot.nix
+++ home/gui/flameshot.nix
@@ -4,16 +4,17 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ flameshot ];
-  xdg.configFile."flameshot/flameshot.ini".text = ''
-    [General]
-    buttons=@Variant(\0\0\0\x7f\0\0\0\vQList<int>\0\0\0\0\x6\0\0\0\xf\0\0\0\b\0\0\0\n\0\0\0\v\0\0\0\f\0\0\0\r)
-    checkForUpdates=false
-    disabledTrayIcon=true
-    drawColor=#ffff00
-    drawThickness=1
-    savePath=${builtins.replaceStrings [ "$HOME" ] [ config.home.homeDirectory ]
-        config.xdg.userDirs.pictures}
-    startupLaunch=false
-  '';
+  services.flameshot = {
+    enable = true;
+    settings.General = {
+      buttons = ''@Variant(\0\0\0\x7f\0\0\0\vQList<int>\0\0\0\0\x6\0\0\0\xf\0\0\0\b\0\0\0\n\0\0\0\v\0\0\0\f\0\0\0\r)'';
+      checkForUpdates = false;
+      disabledTrayIcon = true;
+      drawColor = "#ffff00";
+      drawThickness = "1";
+      savePath = builtins.replaceStrings [ "$HOME" ]
+        [ config.home.homeDirectory ] config.xdg.userDirs.pictures;
+      startupLaunch = false;
+    };
+  };
 }
