d50aa7beba1d395bb258b04d902a6497c82bf96d
Chua Hou <human+github@chuahou.dev>
2022-08-15 10:22:42 +0800

    home/wezterm, flake: use new wezterm colorSchemes option
    - Progresses wezterm on #9.

--- home/gui/wezterm.nix
+++ home/gui/wezterm.nix
@@ -7,24 +7,18 @@
   programs.wezterm = {
     enable = true;
     extraConfig = builtins.readFile ../res/wezterm.lua;
+    colorSchemes.hm-colourscheme = with (import ../lib/gui/colours.nix); rec {
+      foreground = term.fg;
+      background = term.bg;
+      cursor_bg = foreground;
+      cursor_fg = background;
+      cursor_border = cursor_bg;
+      selection_bg = bright.black;
+      selection_fg = bright.white;
+      ansi = [ black red green yellow blue magenta cyan white ];
+      brights = with bright; [
+        black red green yellow blue magenta cyan white
+      ];
+    };
   };
-
-  # Generate colourscheme.
-  xdg.configFile."wezterm/colors/hm-colourscheme.toml".source =
-    (pkgs.formats.toml {}).generate "hm-colourscheme.toml"
-      (with (import ../lib/gui/colours.nix); {
-        colors = rec {
-          foreground = term.fg;
-          background = term.bg;
-          cursor_bg = foreground;
-          cursor_fg = background;
-          cursor_border = cursor_bg;
-          selection_bg = bright.black;
-          selection_fg = bright.white;
-          ansi = [ black red green yellow blue magenta cyan white ];
-          brights = with bright; [
-            black red green yellow blue magenta cyan white
-          ];
-        };
-      });
 }
