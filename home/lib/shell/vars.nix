05bdb5168f550d569b081411aef6955ca575b4f5
Chua Hou <human+github@chuahou.dev>
2023-02-23 11:42:03 +0000

    home/vim: brush up nvim-remote script

--- home/lib/shell/vars.nix
+++ home/lib/shell/vars.nix
@@ -5,10 +5,19 @@
 
 # Environment variables to set wherever needed
 rec {
-  EDITOR = "${pkgs.writeShellScript "nvim-remote" ''
-    [ -z "$NVIM" ] \
-        && exec nvim "$@" \
-        || exec nvim --server $NVIM --remote "$@"
+  EDITOR = "${pkgs.writeShellScript "nvim-remote" /* bash */ ''
+    # If there's exactly 1 argument (assumed to be a filename), and there's an
+    # existing nvim instance, we use --remote to open it in the existing
+    # instance.
+    if [ $# = 1 ] && [ -n "$NVIM" ]; then
+        # We use realpath, as relative paths are interpreted relative to the
+        # directory the existing instance was opened in, which is frequently
+        # incorrect.
+        exec nvim "$@" --server $NVIM --remote $(realpath "$1")
+    else
+        # Otherwise, execute nvim normally.
+        exec nvim "$@"
+    fi
   ''}";
   VISUAL = EDITOR;
 }
