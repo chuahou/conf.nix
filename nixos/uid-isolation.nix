5eac9715f936cd91ec7e9c31c1ebf7d8bfeea184
Chua Hou <human+github@chuahou.dev>
2023-09-17 17:16:34 +0800

    nixos/uid-isolation: isolate Bitwarden

--- nixos/uid-isolation.nix
+++ nixos/uid-isolation.nix
@@ -18,6 +18,11 @@
         binaryName = "Discord";
         user = { name = "discord"; uid = 2002; };
       }
+      {
+        inputDerivation = pkgs.bitwarden;
+        binaryName = "bitwarden";
+        user = { name = "bitwarden"; uid = 2003; };
+      }
     ];
     normalUser = config.users.users.user.name;
   };
