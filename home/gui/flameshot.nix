e4916ecc605c78fee0aab32e6b58d763b8853a12
Chua Hou <human+github@chuahou.dev>
2024-12-12 12:21:20 +0800

    flameshot: remove unrecognized option
    - Also see issue 3718 in flameshot-org/flameshot.

--- home/gui/flameshot.nix
+++ home/gui/flameshot.nix
@@ -8,7 +8,6 @@
     enable = true;
     settings.General = {
       buttons = ''@Variant(\0\0\0\x7f\0\0\0\vQList<int>\0\0\0\0\x6\0\0\0\xf\0\0\0\b\0\0\0\n\0\0\0\v\0\0\0\f\0\0\0\r)'';
-      checkForUpdates = false;
       disabledTrayIcon = true;
       drawColor = "#ffff00";
       drawThickness = "1";
